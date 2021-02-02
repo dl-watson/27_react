87687457cf7f9272d9a1f897234f30d8b4af73ee
dl-watson <dlarkinwatson@gmail.com>
2021-02-02 13:34:24 -0800

    set up List.jsx

---
+++ src/components/list/List.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import ListItem from "./list-item/ListItem";
+
+export default class List extends Component {
+  render() {
+    return (
+      <>
+        <ListItem />
+      </>
+    );
+  }
+}
