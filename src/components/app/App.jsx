2ec010941e3ca90fb6f9553426a4f1a25b8d2f04
dl-watson <dlarkinwatson@gmail.com>
2021-02-02 13:30:00 -0800

    outline App.jsx with Router

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,24 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import Main from "./main/Main.jsx";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-export default function App() {
-  return <h1>Hello World</h1>;
+export default class App extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="main">
+          <Router>
+            <Switch>
+              <Route
+                path="/"
+                exact
+                render={(routerProps) => <Main {...routerProps} />}
+              />
+            </Switch>
+          </Router>
+        </div>
+      </>
+    );
+  }
 }
